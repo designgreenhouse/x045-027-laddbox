13822b8177f471bf4c4fe93af97e8f2fd46bbf0b
Selindmarkl <lars.lindmark@jidoka.se>
2019-05-09 21:35:31 +0200

    Compilerat kod

---
+++ 027-klimatcykel/src/M011-solarxbke.cpp
@@ -0,0 +1,23 @@
+#include "application.h"
+#line 1 "/Users/jido/Documents/GitHub/x045-027-laddbox/027-klimatcykel/src/M011-solarxbke.ino"
+/*
+ * Project 027-klimatcykel
+ * Description:
+ * Author: Lars Lindmark
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "/Users/jido/Documents/GitHub/x045-027-laddbox/027-klimatcykel/src/M011-solarxbke.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
